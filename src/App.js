e304511e7c11c0941a911b58b984269c2c11576f
aicayzer <aicayzer@icloud.com>
2020-03-26 15:14:37 +0000

    add ColorBox

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-
 import "./App.css";
 import Palette from "./Palette";
 import seedColors from "./seedColors";
+
 function App() {
 	return (
 		<div className="App">
-			<Palette />
+			<Palette {...seedColors[1]} />
 		</div>
 	);
 }
--- src/Palette.js
+++ src/Palette.js
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
-import seedColors from "./seedColors";
+import ColorBox from "./ColorBox";
+import "./Palette.css";
 
 class Palette extends Component {
 	state = {};
 	render() {
+		const colorBoxes = this.props.colors.map((color) => <ColorBox background={color.color} name={color.name} />);
 		return (
 			<div className="Palette">
-				<div className="Palette-colors">
-					<h1>Palette</h1>
-				</div>
+				<div className="Palette-colors">{colorBoxes}</div>
 			</div>
 		);
 	}
