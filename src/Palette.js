d4486ab454aa3eb13cd2fc39f0e01f41c2dd7413
aicayzer <aicayzer@icloud.com>
2020-03-26 14:53:40 +0000

    creating palette and adding seedColors

---
+++ src/Palette.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import seedColors from "./seedColors";
+
+class Palette extends Component {
+	state = {};
+	render() {
+		return (
+			<div className="Palette">
+				<div className="Palette-colors">
+					<h1>Palette</h1>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default Palette;
